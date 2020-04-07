3dc5227f64f912743f0b45a686827238f3252c49
phileasdg <pdazeley-gaist23@coa.edu>
2020-04-07 03:53:00 -0400

    Successfully made text adjust to stay in frame from the right of the image when drawing.

--- pygame_pi_UI_test_1.py
+++ pygame_pi_UI_test_1.py
@@ -9,6 +9,7 @@ width = 640
 height = 480
 
 fontSize = 20
+string = "width: " + str(width) + " height : " + str(height) + " size: " + str(fontSize)
 
 # Colours
 WHITE = (255, 255, 255)
@@ -19,11 +20,14 @@ DISPLAYSURF = pygame.display.set_mode((width, height))  # , pygame.FULLSCREEN)
 pygame.display.set_caption("Text display test")
 
 fontObj = pygame.font.Font("freesansbold.ttf", fontSize)  # font, font size
-textSurfaceObj = fontObj.render("width: " + str(width) + " height : " + str(height) +
-                                " size: " + str(fontSize), True, WHITE)
+textSurfaceObj = fontObj.render(string, True, WHITE)
 # text, anti-aliasing, text colour, bg colour
 textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (width - 150, 20)
+
+textWidth = fontObj.size(string)[0]
+textHeight = fontObj.size(string)[1]
+
+textRectObj.center = (width - int(1/2*textWidth) - 10, height - int(1/2*textHeight) - 10)
 
 while True:  # main game loop
     # DISPLAYSURF.fill(WHITE)
