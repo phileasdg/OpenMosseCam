fc8e341324166c352ce0d7cf51322f5002c0a6f3
phileasdg <pdazeley-gaist23@coa.edu>
2020-04-12 02:46:55 -0400

    Progress in the display design on _pi_UI_test_2.py:
    Background UI Grid working.
    Settings Headers appearing in different areas. They need adjustments but at least they are now in their assigned spaces.

--- pygame UI tests/pygame_pi_UI_test_2.py
+++ pygame UI tests/pygame_pi_UI_test_2.py
@@ -22,7 +22,17 @@ lineThickness = int(a/2)
 BACKGROUND= (0, 150, 0)
 WHITE = (255, 255, 255)
 
-settingsList = ["ISO", "Shutter", "Mode", "Effect", "Format", "Resolution", "AWB", "Red:", "Blue:"]
+settingsXY = {
+    "ISO": (a+lineThickness, a+lineThickness),
+    "Shutter": (a+lineThickness+horizontalThird, a+lineThickness),
+    "Mode": (a+lineThickness+2*horizontalThird, a+lineThickness),
+    "Effect": (a+lineThickness, a+lineThickness+verticalThird),
+    "Format": (a+lineThickness+horizontalThird, a+lineThickness+verticalThird),
+    "Resolution": (a+lineThickness+2*horizontalThird, a+lineThickness+verticalThird),
+    "AWB": ((a+lineThickness, a+lineThickness+2*verticalThird)),
+    "Red:": (a+lineThickness+horizontalThird, a+lineThickness+2*verticalThird),
+    "Blue:": (a+lineThickness+2*horizontalThird, a+lineThickness+2*verticalThird),
+    }
 
 pygame.init()
 # pygame.mouse.set_visible(False)
@@ -31,25 +41,26 @@ pygame.display.set_caption("Text display test")
 
 DISPLAYSURF.fill(BACKGROUND )
 
-# background grid
-pygame.draw.rect(DISPLAYSURF, WHITE, (a, a, width - (2*a), height - (2*a)), lineThickness)  # rect tuple (coordinates of A, width, height), line thickness
-pygame.draw.line(DISPLAYSURF, WHITE, (a, a+verticalThird), (width-a, a+verticalThird), lineThickness)  # point A, point B, thickness
-pygame.draw.line(DISPLAYSURF, WHITE, (a, a+2*verticalThird), (width-a, a+2*verticalThird), lineThickness)  # point A, point B, thickness
-pygame.draw.line(DISPLAYSURF, WHITE, (a+horizontalThird, a), (a+horizontalThird, a+2*verticalThird), lineThickness)  # point A, point B, thickness
-pygame.draw.line(DISPLAYSURF, WHITE, (a+2*horizontalThird, a), (a+2*horizontalThird, a+2*verticalThird), lineThickness)  # point A, point B, thickness
+def draw_settings_menu():
+    # background grid
+    pygame.draw.rect(DISPLAYSURF, WHITE, (a, a, width - (2*a), height - (2*a)), lineThickness)  # rect tuple (coordinates of A, width, height), line thickness
+    pygame.draw.line(DISPLAYSURF, WHITE, (a, a+verticalThird), (width-a, a+verticalThird), lineThickness)  # point A, point B, thickness
+    pygame.draw.line(DISPLAYSURF, WHITE, (a, a+2*verticalThird), (width-a, a+2*verticalThird), lineThickness)  # point A, point B, thickness
+    pygame.draw.line(DISPLAYSURF, WHITE, (a+horizontalThird, a), (a+horizontalThird, a+2*verticalThird), lineThickness)  # point A, point B, thickness
+    pygame.draw.line(DISPLAYSURF, WHITE, (a+2*horizontalThird, a), (a+2*horizontalThird, a+2*verticalThird), lineThickness)  # point A, point B, thickness
 
-# display text settings headers
-fontObj = pygame.font.Font("freesansbold.ttf", 40)  # font, font size TODO: figure out formula for font size
+    # display text settings headers
+    fontObj = pygame.font.Font("freesansbold.ttf", 40)  # font, font size TODO: figure out formula for font size
 
-numberOfHorizontalThirds = 0
-numberOfVerticalThirds = 0
+    for k, v in settingsXY.items():
+        k = fontObj.render(k, True, WHITE)  # text, anti-aliasing, text colour, bg colour
+        textRectObj = k.get_rect()
+        textRectObj.topleft = (v)
 
-for i in range(8):
-    i = fontObj.render(settingsList[i], True, WHITE)  # text, anti-aliasing, text colour, bg colour
-    textRectObj = i.get_rect()
-    textRectObj.topleft = (a + lineThickness), (a + lineThickness)
+        DISPLAYSURF.blit(k, textRectObj)
 
-    DISPLAYSURF.blit(i, textRectObj)
+# setup functions calling
+draw_settings_menu()
 
 while True:  # main game loop
     for event in pygame.event.get():
