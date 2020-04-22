02960bb30af6864d7d1f6da12b2c96365ea94119
phileasdg <pdazeley-gaist23@coa.edu>
2020-04-21 21:12:37 -0400

    Updates to Pi menu system.py:
    - sd - added a "3D overlap" option to the "Active Camera(s)" setting in the settings dictionary.
    
    Other updates:
    
    - started learning OpenCV.
    - created directory for documentation of my learning process (for OpenCV)

--- Raspberry Pi programs/Pi menu system.py
+++ Raspberry Pi programs/Pi menu system.py
@@ -154,7 +154,7 @@ sd = {
                  4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2,
                  6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0],
     # TODO: the solution for blue gain and red gain above is pretty poor. To improve.
-    "Active Camera(s)": ["left camera", "right camera", "stereo"]
+    "Active Camera(s)": ["left camera", "right camera", "stereo", "3D overlap"]
 }
 # settings current value index list
 scvil = [
