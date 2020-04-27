d499e8ab8fac72508c62c81f39e158a13d74047b
phileasdg <pdazeley-gaist23@coa.edu>
2020-04-27 03:01:27 -0400

    - Reformated and restructured the program for GUIwithMenu.py to be easier to read and to better integrate the combination of UI and Menu structure. - Added Pygame GUI declarations to settings_menu() to draw settings box, setting names and values (the code works, the design can be improved but there are currently more pressing priorities) - Renamed back() to return_to_viewfinder() - Added Pygame GUI declarations to return_to_viewfinder() to draw over the previous menu. Code is temporary because it does not yet include the camera video stream display. - Added Pygame GUI declaration to next_up() and next_down(): settings_menu() - Wrote function draw_ui_background() which draws the buttons and info UI boxes - Updated sd (settings dictionary) to include draft coordinates for setting names and values in UI - Simplified the Pygame loop section of the program
    To do:
    - Change update_ui_display() to blit pictures or text depending on the setting

--- Raspberry Pi programs/GUIwithMenu.py
+++ Raspberry Pi programs/GUIwithMenu.py
@@ -7,7 +7,13 @@ import sys
 from pygame.locals import *
 
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
-# GPIO AND MENU SETUP:
+# CAMERA SETUP (PICAMERA)
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+
+# TODO: MAKE CAMERA SETUP
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+# GPIO AND FUNCTIONS SETUP:
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 
 buttons = {1: 17, 2: 22, 3: 23, 4: 27}
@@ -35,11 +41,47 @@ def toggle_info():
 
 
 def settings_menu():
+
+    # TODO: make the new drawing discard the old drawing
+    # || Terminal output: ||
     global currentDisplay
     currentDisplay = "settingsMenu"
 
     print("you have pressed 'settings menu'")
-    # change values of the thing (maybe a single function could work)
+
+    # || GUI declarations: ||
+
+    DISPLAYSURF.fill(BLACK)
+    # draw settings box
+    pygame.draw.rect(DISPLAYSURF, WHITE, (marginX, marginY, GUIx, GUIy), 1)  # settings rect
+    pygame.draw.rect(DISPLAYSURF, WHITE, (2 * marginX + GUIx, marginY, 3 * marginX, GUIy + 4 * marginY), 1)  # buttons rect
+    pygame.draw.rect(DISPLAYSURF, WHITE, (marginX, 2 * marginY + GUIy, GUIx, 3 * marginY), 1)  # bottom rect (current setting description)
+
+    for i in range(3):
+        pygame.draw.line(DISPLAYSURF, WHITE, (int(marginX + (i + 1) * x4thSettings), marginY),
+                         (int(marginX + (i + 1) * x4thSettings), GUIy + marginY), 1)  # x4ths
+        pygame.draw.line(DISPLAYSURF, WHITE, (marginX, int(marginY + i * y3rdSettings)),
+                         (GUIx + marginX, int(marginY + i * y3rdSettings)), 1)  # y3rds
+        # point 2 is always GUIsize + margin because we are matching with a rect declared using xy of A followed by
+        # width and height rather than xy of point D
+        pygame.draw.line(DISPLAYSURF, WHITE, (2 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)),
+                         (5 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)), 1)  # y4ths
+
+    # draw settings names
+    for k in sd.keys():
+        fontObj = pygame.font.Font(None, 30)  # font, font size
+        textSurfaceObj = fontObj.render(k, True, WHITE)  # text, anti-aliasing, text colour, bg colour
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.topleft = sd[k][0][0]
+        DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+
+    # draw settings values
+    for setting in sl:
+        fontObj = pygame.font.Font(None, 30)  # font, font size
+        textSurfaceObj = fontObj.render(str(sd[setting][1][scvil[sl.index(setting)]]), True, WHITE)  # text, anti-aliasing, text colour, bg colour
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.topleft = sd[setting][0][1]
+        DISPLAYSURF.blit(textSurfaceObj, textRectObj)
 
 
 def gallery():
@@ -50,11 +92,16 @@ def gallery():
     # change values of the thing (maybe a single function could work)
 
 
-def back():
+def return_to_viewfinder():
     global currentDisplay
     currentDisplay = "viewFinder"
 
     # change the value of the buttons
+
+    # || GUI declarations: ||
+    # TODO: have it draw the image from the viewfinder over the display
+    DISPLAYSURF.fill(BLACK) # TODO: Replace with camera overlay
+    draw_ui_background()
 
 
 def next_up():
@@ -72,6 +119,9 @@ def next_up():
     else:  # can only be called from the gallery
         print("next picture up")
 
+    # || GUI declarations: ||
+    settings_menu()
+
 
 def next_down():
     if currentDisplay == "settingsMenu":
@@ -84,6 +134,9 @@ def next_down():
         print(sl[csi] + " = " + str(sd[sl[csi]][1][scvil[csi]]))
     else:  # can only be called from the gallery
         print("next picture down")
+
+    # || GUI declarations: ||
+    settings_menu()
 
 
 def change_setting():
@@ -100,19 +153,57 @@ def delete():
     print("content deleted")
 
 
+def draw_ui_background():
+    pygame.draw.rect(DISPLAYSURF, WHITE, (2 * marginX + GUIx, marginY, 3 * marginX, GUIy + 4 * marginY), 1)  # buttons rect
+    pygame.draw.rect(DISPLAYSURF, WHITE, (marginX, 2 * marginY + GUIy, GUIx, 3 * marginY), 1)  # bottom rect (current setting description)
+    for i in range(3):
+        pygame.draw.line(DISPLAYSURF, WHITE, (2 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)), (5 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)), 1)  # y4ths
+
+
 def update_ui_display():
     # button function descriptions print
     for i in range(4):
         print(displayDescriptions[currentDisplay]["Button" + str(i + 1)])
     print("please press a button:")
 
-    for k in sd.keys():
-        fontObj = pygame.font.Font(None, 30)  # font, font size
-        textSurfaceObj = fontObj.render(k, True, WHITE)  # text, anti-aliasing, text colour, bg colour
-        textRectObj = textSurfaceObj.get_rect()
-        textRectObj.topleft = sd[k][0][0]
-        DISPLAYSURF.blit(textSurfaceObj, textRectObj)
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+# PYGAME SETUP:
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 
+# // Display settings
+pygame.display.init()
+info = pygame.display.Info()
+dX, dY = info.current_w, info.current_h  # display resolution
+# dX, dY = 640, 480 # use if you want to set the display res to custom values
+marginX = int(dX / 32)
+marginY = int(dY / 32)
+
+# // GUI settings
+GUIx, GUIy = int(dX - 6 * marginX), int(dY - 6 * marginY)  # GUI display size (dX or dY - at least 2* margin of X or Y)
+# GUI top left = marginX, marginY
+
+# // UI BUILDING BLOCKS:
+# Settings block (the settings rectangle):
+x4thSettings = (GUIx / 4)
+y3rdSettings = (GUIy / 3)
+# Settings cell:
+# TODO: Button cell useful references to build icons
+# Buttons Block:
+y4thButtons = ((GUIy + 4 * marginY) / 4)
+
+# // Start Pygame
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((dX, dY))  # , pygame.FULLSCREEN)
+pygame.display.set_caption("Text display test") # TODO: change
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+# BLUE = (0, 0, 128)
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+# MENU STRUCTURE AND SETTINGS SETUP
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 
 # Dictionaries and lists (camera settings and UI)
 displayFunctions = {
@@ -123,13 +214,13 @@ displayFunctions = {
         "Button4": gallery
     },
     "settingsMenu": {
-        "Button1": back,
+        "Button1": return_to_viewfinder,
         "Button2": next_up,
         "Button3": next_down,
         "Button4": change_setting
     },
     "gallery": {
-        "Button1": back,
+        "Button1": return_to_viewfinder,
         "Button2": next_up,
         "Button3": next_down,
         "Button4": delete
@@ -160,39 +251,42 @@ displayDescriptions = {
 sl = ["Mode", "Active Camera(s)", "Shutter", "ISO", "AWB", "Red gain", "Blue gain", "Effect", "Resolution", "Crop", "Format", "fps"]  # TODO: framerate and crop
 
 sd = {
+    # TODO: reformat and clean up when finished
     # "Reference": [["key blit xy (x, y)", "value blit xy (x, y)"], ["list of setting values"]], TODO!!!!!!!!!!!!!!!!!!
-    "ISO": [[(100, 100), (200, 200)], [0, 100, 160, 200, 250, 320, 400, 500, 640, 800]],  # limited number of values
-    "Shutter": [[(100, 100), (200, 200)], [("auto", 0), ("1/8000", 125), ("1/6400", 156), ("1/5000", 200), ("1/4000", 250),
-                ("1/3200", 312), ("1/2500", 400), ("1/2000", 500), ("1/1600", 625), ("1/1250", 800), ("1/1000", 1000),
-                ("1/800", 1250), ("1/640", 1562), ("1/500", 2000), ("1/400", 2500), ("1/320", 3125), ("1/250", 4000),
-                ("1/200", 5000), ("1/160", 6250), ("1/125", 8000), ("1/100", 10000), ("1/80", 12500), ("1/60", 16666),
-                ("1/50", 20000), ("1/40", 25000), ("1/30", 33333), ("1/25", 40000), ("1/20", 50000), ("1/15", 66666),
-                ("1/13", 76923), ("1/10", 100000), ("1/8", 125000), ("1/6", 166666), ("1/5", 200000), ("1/4", 250000),
-                ("0.3", 300000), ("0.4", 400000), ("0.5", 500000), ("0.6", 600000), ("0.8", 800000), ("1", 1000000),
-                ("1.3", 1300000), ("1.6", 1600000), ("2", 2000000), ("2.5", 2500000), ("3.2", 3200000), ("4", 4000000),
-                ("5", 5000000), ("6", 6000000)]],  # values seen by user and values read by program are different
-    "Mode": [[(100, 100), (200, 200)], ["photo", "video"]],  # only two modes, could be tied to a boolean
-    "Effect": [[(100, 100), (200, 200)], ["none", "negative", "solarize", "hatch", "gpen", "film", "colorswap", "washedout",
-               "colorbalance", "cartoon"]],  # limited number of values
-    # effects availablee: 'none', 'negative', 'solarize', 'sketch', 'denoise', 'emboss', 'oilpaint', 'hatch', 'gpen',
-    # 'pastel', 'watercolor', 'film', 'blur', 'saturation', 'colorswap', 'washedout', 'posterise', 'colorpoint',
-    # 'colorbalance', 'cartoon', 'deinterlace1', 'deinterlace2'
-    "Format": [[(100, 100), (200, 200)], ["jpg", "png"]],  # limited number of values
-    "Resolution": [[(100, 100), (200, 200)], [(1980, 1080), (1080, 720)]],  # limited number of tuples, different
-    "AWB": [[(100, 100), (200, 200)], ["off", "greyworld", "auto", "sunlight", "cloudy", "shade", "tungsten", "fluorescent", "incandescent",
-            "flash", "horizon"]],
-    "Red gain": [[(100, 100), (200, 200)], [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
+    "Mode": [[(marginX, marginY), (marginX, marginY+(y3rdSettings/2))], ["photo", "video"]],
+    "Active Camera(s)": [[(marginX+x4thSettings, marginY),(marginX+x4thSettings, marginY+(y3rdSettings/2))], ["left camera", "right camera", "stereo", "3D overlap"]],
+    "Shutter": [[(marginX+(2*x4thSettings), marginY), (marginX+(2*x4thSettings), marginY+(y3rdSettings/2))], [("auto", 0), ("1/8000", 125), ("1/6400", 156),
+                ("1/5000", 200), ("1/4000", 250), ("1/3200", 312), ("1/2500", 400), ("1/2000", 500), ("1/1600", 625),
+                ("1/1250", 800), ("1/1000", 1000), ("1/800", 1250), ("1/640", 1562), ("1/500", 2000), ("1/400", 2500),
+                ("1/320", 3125), ("1/250", 4000), ("1/200", 5000), ("1/160", 6250), ("1/125", 8000), ("1/100", 10000),
+                ("1/80", 12500), ("1/60", 16666), ("1/50", 20000), ("1/40", 25000), ("1/30", 33333), ("1/25", 40000),
+                ("1/20", 50000), ("1/15", 66666), ("1/13", 76923), ("1/10", 100000), ("1/8", 125000), ("1/6", 166666),
+                ("1/5", 200000), ("1/4", 250000), ("0.3", 300000), ("0.4", 400000), ("0.5", 500000), ("0.6", 600000),
+                ("0.8", 800000), ("1", 1000000), ("1.3", 1300000), ("1.6", 1600000), ("2", 2000000), ("2.5", 2500000),
+                ("3.2", 3200000), ("4", 4000000), ("5", 5000000), ("6", 6000000)]],
+    "ISO": [[(marginX+(3*x4thSettings), marginY), (marginX+(3*x4thSettings), marginY+(y3rdSettings/2))], [0, 100, 160, 200, 250, 320, 400, 500, 640, 800]],
+    "AWB": [[(marginX, marginY+y3rdSettings), (marginX, marginY+y3rdSettings+(y3rdSettings/2))], ["off", "greyworld", "auto", "sunlight", "cloudy", "shade", "tungsten",
+                                       "fluorescent", "incandescent", "flash", "horizon"]],
+    # TODO: the solution for blue gain and red gain above is pretty poor. To improve.
+    "Red gain": [[(marginX+x4thSettings, marginY+y3rdSettings), (marginX+x4thSettings, marginY+y3rdSettings+(y3rdSettings/2))], [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
                  2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1,
                  4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2,
                  6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0]],
-    "Blue gain": [[(100, 100), (200, 200)], [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
+    "Blue gain": [[(marginX+(2*x4thSettings), marginY+y3rdSettings), (marginX+(2*x4thSettings), marginY+y3rdSettings+(y3rdSettings/2))], [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0,
                   2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1,
                   4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2,
                   6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0]],
-    # TODO: the solution for blue gain and red gain above is pretty poor. To improve.
-    "Active Camera(s)": [[(100, 100), (200, 200)], ["left camera", "right camera", "stereo", "3D overlap"]],
-    "fps": [[(100, 100), (200, 200)], [30]],  # TODO
-    "Crop": [[(100, 100), (200, 200)], [(0, 0)]]  # TODO
+    "Effect": [[(marginX+(3*x4thSettings), marginY+y3rdSettings), (marginX+(3*x4thSettings), marginY+y3rdSettings+(y3rdSettings/2))], ["none", "negative", "solarize", "hatch", "gpen", "film", "colorswap",
+                                          "washedout", "colorbalance", "cartoon"]],
+    # effects availablee: 'none', 'negative', 'solarize', 'sketch', 'denoise', 'emboss', 'oilpaint', 'hatch', 'gpen',
+    # 'pastel', 'watercolor', 'film', 'blur', 'saturation', 'colorswap', 'washedout', 'posterise', 'colorpoint',
+    # 'colorbalance', 'cartoon', 'deinterlace1', 'deinterlace2'
+
+    "Resolution": [[(marginX, marginY+(2*y3rdSettings)), (marginX, marginY+(2*y3rdSettings)+(y3rdSettings/2))], [(1980, 1080), (1080, 720)]],  # limited number of tuples, different
+    "Crop": [[(marginX+x4thSettings, marginY+(2*y3rdSettings)), (marginX+x4thSettings, marginY+(2*y3rdSettings)+(y3rdSettings/2))], [(0, 0)]],  # TODO
+    "Format": [[(marginX+(2*x4thSettings), marginY+(2*y3rdSettings)), (marginX+(2*x4thSettings), marginY+(2*y3rdSettings)+(y3rdSettings/2))], ["jpg", "png"]],
+    "fps": [[(marginX+(3*x4thSettings), marginY+(2*y3rdSettings)), (marginX+(3*x4thSettings), marginY+(2*y3rdSettings)+(y3rdSettings/2))], [30]]  # TODO
+
 }
 # settings current value index list
 scvil = [
@@ -211,64 +305,14 @@ scvil = [
 ]
 
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
-# PYGAME SETUP:
-# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
-
-# // Display settings
-pygame.display.init()
-info = pygame.display.Info()
-dX, dY = info.current_w, info.current_h  # display resolution
-# dX, dY = 640, 480 # use if you want to set the display res to custom values
-marginX = int(dX / 32)
-marginY = int(dY / 32)
-
-# // GUI settings
-GUIx, GUIy = int(dX - 6 * marginX), int(dY - 6 * marginY)  # GUI display size (dX or dY - at least 2* margin of X or Y)
-# GUI top left = marginX, marginY
-
-# // UI BUILDING BLOCKS:
-# Settings block:
-x4thSettings = (GUIx / 4)
-y3rdSettings = (GUIy / 3)
-# Settings cell:
-# TODO: Button cell useful references to build icons
-# Buttons Block:
-y4thButtons = ((GUIy + 4 * marginY) / 4)
-
-# // Start Pygame
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((dX, dY))  # , pygame.FULLSCREEN)
-pygame.display.set_caption("Text display test") # TODO: change
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-# BLUE = (0, 0, 128)
-
-# // Initial UI drawing declarations:
-# TODO: put this in a function and run it once.
-# Draw Background
-pygame.draw.rect(DISPLAYSURF, WHITE, (marginX, marginY, GUIx, GUIy), 1)  # settings rect
-pygame.draw.rect(DISPLAYSURF, WHITE, (2 * marginX + GUIx, marginY, 3 * marginX, GUIy + 4 * marginY), 1)  # buttons rect
-pygame.draw.rect(DISPLAYSURF, WHITE, (marginX, 2 * marginY + GUIy, GUIx, 3 * marginY),
-                 1)  # bottom rect (current setting description)
-for i in range(3):
-    pygame.draw.line(DISPLAYSURF, WHITE, (int(marginX + (i + 1) * x4thSettings), marginY),
-                     (int(marginX + (i + 1) * x4thSettings), GUIy + marginY), 1)  # x4ths
-    pygame.draw.line(DISPLAYSURF, WHITE, (marginX, int(marginY + i * y3rdSettings)),
-                     (GUIx + marginX, int(marginY + i * y3rdSettings)), 1)  # y3rds
-    # point 2 is always GUIsize + margin because we are matching with a rect declared using xy of A followed by
-    # width and height rather than xy of point D
-    pygame.draw.line(DISPLAYSURF, WHITE, (2 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)),
-                     (5 * marginX + GUIx, int(marginY + (i + 1) * y4thButtons)), 1)  # y4ths
-
-# blit initial settings
-update_ui_display()
-# TODO: make the update_ui_display() overwrite the current display rather than add to it.
-
-# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
 # PYGAME LOOP
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////// #
+
+# // Initial UI drawing:
+
+draw_ui_background()
+update_ui_display()
+# TODO: make the update_ui_display() overwrite the current display rather than add to it.
 
 while True:  # main game loop
     # game code here:
@@ -280,8 +324,6 @@ while True:  # main game loop
             displayFunctions[currentDisplay]["Button" + str(k)]()
             update_ui_display()
             sleep(0.3)
-
-
 
     for event in pygame.event.get():
         if event.type == QUIT:
